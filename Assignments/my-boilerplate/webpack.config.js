ab6c33edbf45dba5d20fc3297ddc7e73921dca7d
smankow <codarus77@gmail.com>
2022-04-08 13:53:42 -0400

    scss working

--- Assignments/my-boilerplate/webpack.config.js
+++ Assignments/my-boilerplate/webpack.config.js
@@ -17,8 +17,19 @@ module.exports = {
           loader: "babel-loader",
           options: {
             presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
+          }
+        }
+      },
+      {
+            test: /\.s[ac]ss$/i,
+            use: [
+              // Creates `style` nodes from JS strings
+              "style-loader",
+              // Translates CSS into CommonJS
+              "css-loader",
+              // Compiles Sass to CSS
+              "sass-loader",
+            ],
       },
     ],
   },
