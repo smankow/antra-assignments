c1ed9d72a4f24febe50903b52fcdfdb061361962
smankow <codarus77@gmail.com>
2022-04-29 15:23:02 -0400

    commit components added

--- Assignments/MySiteAssignment/mysite-cra/src/App.js
+++ Assignments/MySiteAssignment/mysite-cra/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import './style.css'
+import './boostrap/css/bootstrap.min.css'
+import Header from './components/Header';
+import Main from './components/Main';
+import About from './components/About'
+import Summary from './components/Summary';
+import Projects from './components/Projects';
+import Contact from './components/Contact'
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main/>
+      <About/>
+      <Summary/>
+      <Projects/>
+      <Contact />
+      <Footer />
     </div>
   );
 }
