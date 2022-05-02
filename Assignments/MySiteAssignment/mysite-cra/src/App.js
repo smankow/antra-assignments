d1592e94ba54c93776680290df207748cd8f9912
smankow <codarus77@gmail.com>
2022-05-02 14:35:45 -0400

    removed comments

--- Assignments/MySiteAssignment/mysite-cra/src/App.js
+++ Assignments/MySiteAssignment/mysite-cra/src/App.js
@@ -1,7 +1,5 @@
-// import logo from './logo.svg';
-// import './App.css';
+import './boostrap/css/bootstrap.css'
 import './style.css'
-import './boostrap/css/bootstrap.min.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navigation from './components/Navigation/Navigation';
@@ -11,13 +9,12 @@ import Summary from './components/Summary/Summary';
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer';
-// import MainPage from './layouts/MainPage';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navigation></Navigation>
+        <Navigation/>
         <div className="content">
 
           <Routes>
